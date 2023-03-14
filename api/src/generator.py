9ad406a4b60de83ae5115fd84f2aead17b9b77af
swewalka-tgm <swewalka@student.tgm.ac.at>
2023-03-14 14:37:20 +0100

    added openai org name to env variables

--- api/src/generator.py
+++ api/src/generator.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import create_access_token, create_refresh_token, jwt_re
 
 generator = Blueprint('generator',__name__, url_prefix='/api/v1/generator')
 
-openai.organization = "org-qzx7KtwC7edug82IJeee0SmF"
+openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def get_video_id(url):
@@ -21,7 +21,6 @@ def get_video_id(url):
         abort(404, "Video not available")
 
     return extract.video_id(url)
-
 def transcribe_video(video_id):
 
     try:
